648d26f38c323c0cecbe28bc994fe702343fcce9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-30 11:42:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if !params[:name] || params[:name].empty?
+      redirect_to(controller: 'sessions',action:'new')
+    else
+      session[:name] = params[:name]
+      redirect_to(controller: 'application',action:'hello')      
+    end  
   end
 
   def destroy
